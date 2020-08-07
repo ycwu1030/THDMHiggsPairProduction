3b2fadec3ce7c8b7ec9e3f6cd80c949d2b6488ec
Yongcheng Wu <yc_wu1030@126.com>
2020-08-06 23:03:32 -0400

    Update(ScanCS)
    update ScanCS script

--- MMA/ScanCS.m
+++ MMA/ScanCS.m
@@ -1,4 +1,3 @@
-#!/usr/bin/env wolframscript
 (* ::Package:: *)
 
 Install["LoopTools"];
@@ -24,6 +23,20 @@ Get[PROCSRCNAME[[proc]][[1]]];
 Get[PROCSRCNAME[[proc]][[2]]];
 
 
-(*FuncPtr={{ggtohhLHC14I, ggtohhLHC14II}, {ggtohHLHC14I, ggtohHLHC14II}};
-Func2BeRun=FuncPtr[[proc]][[type]];*)
-Print[type,proc];
+FuncPtr={{ggtohhLHC14I, ggtohhLHC14II}, {ggtohHLHC14I, ggtohHLHC14II}};
+Func2BeRun=FuncPtr[[proc]][[type]];
+Print[ToString[Func2BeRun]];
+Print["Type: ",type];
+Print["Process:",proc];
+
+
+str=OpenWrite["CSdata.dat"];
+tbList={0.5,1.0,5.0,10.0};
+For[itb=1,itb<=Length[tbList],itb++,
+	tb=tbList[[itb]];
+	For[m12=0,m12<=200,m12+=10,
+	Print["Calculating CS for: ","MHL=",MHL," tb=",tb," m12=",m12];
+	cs=Func2BeRun[MHL,125.0,m12,ArcTan[tb],ArcTan[tb]]//Quiet;
+	Write[str,StringRiffle[ToString/@{MHL,tb,m12,cs},{"","\t","\n"}]];
+	]
+]
